4f0da6854def6155a5947dd3f524208209f0e716
jasondsouza22 <jasondsouza2211@gmail.com>
2023-08-22 16:56:57 +0530

    Buffer_600m

--- buffer.py
+++ buffer.py
@@ -3,6 +3,7 @@ import arcpy
 arcpy.env.workspace = r"D:\Second Year\Sem 3\Programming_for_GIS_III\P1_Running_Python_Scripts\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
 
 # Performing buffer operations
-arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer","500 meters")
+#arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer","500 meters")
 
+arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer_600","600 meters")
 print("Process Completed")
