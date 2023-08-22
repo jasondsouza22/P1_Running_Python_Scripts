7466aabbd27b7a85f8c8e1cdad1b83a425526145
jasondsouza22 <jasondsouza2211@gmail.com>
2023-08-22 15:23:08 +0530

    Types

--- first.py
+++ first.py
@@ -5,3 +5,20 @@ print(name)
 print(surname)
 
 print("My first name is {} and my surname is {}".format(name, surname))
+
+age = "21"
+print(type(age))
+print(type(surname))
+
+# Converting string to integer
+age = int(age)
+print(type(age))
+
+# Converting integer to string
+age = str(age)
+print(type(age))
+
+true_boolean = True
+false_boolean = False
+
+print(type(true_boolean))
