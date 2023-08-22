360f5e43e24c48fc1e4ec7429ae82c2ef1ec7151
jasondsouza22 <jasondsouza2211@gmail.com>
2023-08-22 16:18:59 +0530

    Buffer for Schools

--- buffer.py
+++ buffer.py
@@ -1,3 +1,8 @@
 import arcpy
 
 arcpy.env.workspace = r"D:\Second Year\Sem 3\Programming_for_GIS_III\P1_Running_Python_Scripts\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
+
+# Performing buffer operations
+arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer","500 meters")
+
+print("Process Completed")
