bac83058e8199ef9bbb54568619d647c8a5cab30
jasondsouza22 <jasondsouza2211@gmail.com>
2023-08-29 14:25:16 +0530

    append and insert

--- lists.py
+++ lists.py
@@ -4,3 +4,13 @@ cities = ["Mumbai","Pune","Bangalore","Chennai"]
 print(cities)
 print(type(cities))
 print(cities[1])
+
+#append
+cities.append("Nagpur")
+cities.append(True)
+cities.append(False)
+print(cities)
+
+#insert
+cities.insert(1,"Delhi")
+print(cities)
