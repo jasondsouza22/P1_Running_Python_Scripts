48636e99001c7c8083d22499913877c39c430d03
jasondsouza22 <jasondsouza2211@gmail.com>
2023-08-29 14:46:57 +0530

    Append, Insert, Pop, Loop

--- lists.py
+++ lists.py
@@ -12,5 +12,28 @@ cities.append(False)
 print(cities)
 
 #insert
-cities.insert(1,"Delhi")
+cities.insert(3,"Delhi")
 print(cities)
+
+# Removing element from the end of the list
+cities.pop()
+
+# Removing element by using the index
+cities.pop(3)
+cities.pop()
+
+print(cities)
+
+for city in cities:
+    print("The current value is:")
+    print(city)
+
+print("Process Completed")
+
+
+
+
+Clubs = ["Manchester United", "Chelsea","Arsenal","Newcastle"]
+for club in Clubs:
+    print(club)
+
