d89cd74553a807cdd8de29c9ea7eab4ecfb44ca6
jasondsouza22 <jasondsouza2211@gmail.com>
2023-08-29 14:12:51 +0530

    operations

---
+++ arithmetic_operations.py
@@ -0,0 +1,16 @@
+n1 = 8
+n2 = 16
+
+
+#addition
+add = n1 + n2;
+print ("The addition of {} and {} is {}".format(n1,n2,add))
+
+sub = n1 - n2;
+print ("The subtraction of {} and {} is {}".format(n1,n2,sub))
+
+multi = n1 * n2;
+print ("The multiplication of {} and {} is {}".format(n1,n2,multi))
+
+div = n1 / n2;
+print ("The divison of {} and {} is {}".format(n1,n2,div))
